be01cdf2ffba4ae41030631c04f9b329a45c1a49
Asen Krasimirov <Asen.Krasimirov@fourth.com>
2024-12-26 22:10:39 +0200

    Fix app.py lint

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 import socket
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return "Hello! I am a Flask application running on {}".format(socket.gethostname())
+    host_name = socket.gethostname()
+    return f"Hello! I am a Flask application running on {host_name}"
 
 
 if __name__ == '__main__':
